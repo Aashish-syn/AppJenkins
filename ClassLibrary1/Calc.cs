ced6b9585084458cf8aa9ea4edf82ea17aa0a64c
aashish-syn <firedetection@gmail.com>
2022-08-12 14:31:50 +0530

    Add project files.

---
+++ ClassLibrary1/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyCalc
+{
+    public class Calc
+    {
+        public int add(int x,int y)
+        {
+            return x + y;
+        }
+        public int multiply(int x,int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ helloapp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyCalc;
+namespace helloapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jenkins");
+            Calc calc = new Calc();
+            Console.WriteLine($"Multiply = {calc.multiply(4,5)}");
+            Console.ReadLine();
+        }
+    }
+}
